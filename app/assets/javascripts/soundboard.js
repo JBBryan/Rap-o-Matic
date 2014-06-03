6fc60b98818efc116e73507c3d8d01781f94feef
Brady Bryan <jbraden.bryan@gmail.com>
2014-06-02 21:49:29 -0500

    Adds image overlays (ugh). Adds styling. Adds jQuery hover effect.

--- app/assets/javascripts/soundboard.js
+++ app/assets/javascripts/soundboard.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+$(".rapper-container").hover(function(){
+  $('img', this).first().toggleClass('hidden', [1000])
+})
+
 $(".rapper-container").on('click', function(){
   $('audio', this).get(0).play()
 })
